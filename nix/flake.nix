88d9d88b8c3863d7c3968c4e87ad6f4a8e97a58c
Pawel Lisewski <dev@palekiwi.com>
2026-06-02 16:24:26 +0800

    feat(nix): wire up git config and ignore in dev shell
    - Move git identity from cast.env to nix/.gitconfig
    - Use GIT_CONFIG_COUNT in flake.nix to inject config and ignore files
    - Remove redundant GIT_* variables from cast.env

--- nix/flake.nix
+++ nix/flake.nix
@@ -4,12 +4,12 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    mem.url = "github:palekiwi-labs/mem/f26675a35f592e561dbc4c61a4299c2b35af8db0";
+    mem.url = "github:palekiwi-labs/mem/8f190c989743ead53009b3e610cfd791a04555b0";
     cast.url = "github:palekiwi-labs/cast/dev";
     nvf.url = "github:NotAShelf/nvf";
   };
 
-  outputs = { nixpkgs, flake-utils, nvf, cast, ... }@inputs:
+  outputs = { nixpkgs, flake-utils, nvf, ... }@inputs:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
@@ -32,9 +32,19 @@
           ];
 
           shellHook = ''
+            # Wire up git configuration
+            export GIT_CONFIG_COUNT=2
+            export GIT_CONFIG_KEY_0="include.path"
+            export GIT_CONFIG_VALUE_0="${./.gitconfig}"
+            export GIT_CONFIG_KEY_1="core.excludesFile"
+            export GIT_CONFIG_VALUE_1="${./.gitignore}"
+
             echo "CAST Global Nix Environment Loaded"
 
             export EDITOR=nvim
+
+            export MEM_ARTIFACT_TYPES='["spec", "plan", "trace", "doc", "todo", "bin", "tmp", "ref"]'
+            export MEM_IGNORED_TYPES='["tmp", "ref"]'
           '';
         };
       }
