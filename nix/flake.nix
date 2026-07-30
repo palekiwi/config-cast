1cfb5b270caf3e2126193488e76dc34b9166ba9a
Pawel Lisewski <dev@palekiwi.com>
2026-07-30 21:23:05 +0800

    tmp: add pinned cast-agent

--- nix/flake.nix
+++ nix/flake.nix
@@ -13,7 +13,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     cue.url = "github:palekiwi-labs/cue";
-    cast.url = "github:palekiwi-labs/cast";
+    cast.url = "github:palekiwi-labs/cast/2b25028b6cdcb4ff1a8d8dbb1624276fb2656a8d";
     nvf.url = "github:NotAShelf/nvf";
     llm-agents = {
       url = "github:numtide/llm-agents.nix";
@@ -43,6 +43,7 @@
           tree-sitter
 
           inputs.cue.packages.${system}.cue
+          inputs.cast.packages.${system}.cast-agent
           inputs.cast.packages.${system}.cast-mcp-client
         ];
 
